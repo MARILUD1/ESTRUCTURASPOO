fe148b9038f8b98fb0ee3d13b62b5b8a22d6258a
Marilud1 <cm.castilloc@uea.edu.ec>
2025-01-21 10:45:38 -0500

    Inicializacion de la clase Nodo

--- ESTRUCTURAS_TERCER_NIVEL/Program.cs
+++ ESTRUCTURAS_TERCER_NIVEL/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Bienvenido a mis proyectos UEA");
+
+public class Nodo{
+  private int dato;
+  private Nodo Siguente;
+    public Nodo(int Dato){
+    this.dato = Dato; 
+    this. Siguente = null;  
+}
+}
